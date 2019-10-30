ab14599ca8c077a62610fc3e8231a47e5e27dca7
OldenErwanin <51171007+OldenErwanin@users.noreply.github.com>
2019-10-30 19:47:35 +0000

    Add files via upload

---
+++ index.php
@@ -0,0 +1,42 @@
+<?php include "header.html" ?>
+
+<main>
+    <div id="about">
+        <div class="section-title" data-aos="zoom-in" data-aos-duration="1200">
+            <h2>About me</h2>
+        </div>
+    </div>
+    <div id="references">
+        <div class="section-title" data-aos="zoom-in" data-aos-duration="1200">
+            <h2>References</h2>
+        </div>
+        <div class="container">
+            <div class="body">
+                <div class="hide">
+                    <p class="title">Kingdoms Roleplay</p>
+                    <p class="tech">Used technologies:<br><span>HTML, CSS, PHP</span></p>
+                    <p class="message">Message:<br><span>adsads sadsaidj  sisis s s sidi  sii o lape</span></p>
+                    <form class="source" action="">
+                        <button type="submit"><div class="btn-text">View source</div></button>
+                    </form>
+                    <form class="live" action="">
+                        <button type="submit"><div class="btn-text">Live review</div></button>
+                    </form>
+                </div>
+                <img src="img/background.jpg" alt="">
+            </div>
+            <div class="body">
+                asd
+            </div>
+            <div class="body">
+                asd
+            </div>
+            <div class="body">
+                asd
+            </div>
+        </div>
+    </div>
+</main>
+<script>
+    
+</script>
